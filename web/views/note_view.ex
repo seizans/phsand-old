f7fbc09231c1e7391e137507b8a26a01332c9150
Seizan Shimazaki <seizans@gmail.com>
2016-04-25 15:21:48 +0900

    json response の形式に data: を入れないようにする

--- web/views/note_view.ex
+++ web/views/note_view.ex
@@ -2,11 +2,11 @@ defmodule Phsand2.NoteView do
   use Phsand2.Web, :view
 
   def render("index.json", %{notes: notes}) do
-    %{data: render_many(notes, Phsand2.NoteView, "note.json")}
+    render_many(notes, Phsand2.NoteView, "note.json")
   end
 
   def render("show.json", %{note: note}) do
-    %{data: render_one(note, Phsand2.NoteView, "note.json")}
+    render_one(note, Phsand2.NoteView, "note.json")
   end
 
   def render("note.json", %{note: note}) do
